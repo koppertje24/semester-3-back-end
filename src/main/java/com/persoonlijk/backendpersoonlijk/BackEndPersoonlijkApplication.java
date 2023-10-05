2c6fb550d75848ad5ab6829086ee8eeb60057b42
Koppers,Anne A.E.J <a.koppers@student.fontys.nl>
2023-10-05 09:42:18 +0200

    setup back-end

---
+++ src/main/java/com/persoonlijk/backendpersoonlijk/BackEndPersoonlijkApplication.java
@@ -0,0 +1,13 @@
+package com.persoonlijk.backendpersoonlijk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPersoonlijkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndPersoonlijkApplication.class, args);
+	}
+
+}
