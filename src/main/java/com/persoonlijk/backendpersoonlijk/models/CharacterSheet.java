16e3a4a2093365265ed416ca87e4f213b6fcc168
Koppers,Anne A.E.J <a.koppers@student.fontys.nl>
2023-10-05 11:52:08 +0200

    first working run with the database

---
+++ src/main/java/com/persoonlijk/backendpersoonlijk/models/CharacterSheet.java
@@ -0,0 +1,15 @@
+package com.persoonlijk.backendpersoonlijk.models;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class CharacterSheet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "CharacterName")
+    private String CharacterName;
+}
