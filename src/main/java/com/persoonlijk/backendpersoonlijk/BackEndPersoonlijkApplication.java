aaa001d36a03b253fc0dfe47f3467faa1f4ccfd3
Koppers,Anne A.E.J <a.koppers@student.fontys.nl>
2023-10-06 16:17:53 +0200

    config update

--- src/main/java/com/persoonlijk/backendpersoonlijk/BackEndPersoonlijkApplication.java
+++ src/main/java/com/persoonlijk/backendpersoonlijk/BackEndPersoonlijkApplication.java
@@ -2,9 +2,6 @@ package com.persoonlijk.backendpersoonlijk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEndPersoonlijkApplication {
@@ -13,14 +10,5 @@ public class BackEndPersoonlijkApplication {
 		SpringApplication.run(BackEndPersoonlijkApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
 
 }
---
+++ src/main/java/com/persoonlijk/backendpersoonlijk/configuration.java
@@ -0,0 +1,20 @@
+package com.persoonlijk.backendpersoonlijk;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class configuration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:3000"); // Voeg hier de URL van je frontend toe
+            }
+        };
+    }
+}
