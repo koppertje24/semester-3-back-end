396b0cf8f9db5898ef432f1c6b23898b0d5e1631
koppertje24 <32428243+koppertje24@users.noreply.github.com>
2023-10-10 10:36:32 +0200

    connection fix

--- src/main/java/com/persoonlijk/backendpersoonlijk/configuration.java
+++ src/main/java/com/persoonlijk/backendpersoonlijk/configuration.java
@@ -1,20 +1,20 @@
 package com.persoonlijk.backendpersoonlijk;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class configuration {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:3000"); // Voeg hier de URL van je frontend toe
-            }
-        };
+@EnableWebMvc
+public class configuration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
+                .maxAge(3600); // Max age for preflight (OPTIONS) requests
     }
 }
