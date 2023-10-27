502e531dc9568305aac535cd48fbaedb400a2b05
laptop-002 <annekoppersdev@gmail.com>
2023-10-27 16:07:46 +0200

    addid the classes

--- src/main/java/com/persoonlijk/backendpersoonlijk/DatabaseModels/CharacterSheet.java
+++ src/main/java/com/persoonlijk/backendpersoonlijk/DatabaseModels/CharacterSheet.java
@@ -6,12 +6,32 @@ import lombok.Data;
 @Data
 public class CharacterSheet {
 
+    enum EnumCharacterlass {
+        NoneSelecter,
+        Barbarian,
+        Bard,
+        Cleric,
+        Druid,
+        Fighter,
+        Monk,
+        Paladin,
+        Ranger,
+        Rogue,
+        Sorcerer,
+        Warlock,
+        Wizard,
+        Artificer,
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "characterName")
     private String CharacterName;
+
+    @Column(name = "characterClass")
+    private EnumCharacterlass CharacterClass;
 
     @Column(name = "playerId")
     private Long playerId;
