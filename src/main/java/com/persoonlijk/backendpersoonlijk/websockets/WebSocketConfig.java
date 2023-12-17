9f11b703d288c366b6700ffc3e173d344fa59ac6
laptop-002 <annekoppersdev@gmail.com>
2023-12-17 18:06:32 +0100

    build fix

--- src/main/java/com/persoonlijk/backendpersoonlijk/websockets/WebSocketConfig.java
+++ src/main/java/com/persoonlijk/backendpersoonlijk/websockets/WebSocketConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
